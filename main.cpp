414faa3fac788a9845196199350e0100fc5dfec0
chicchikori <c.takahashi.25.1.1645@4nonome.com>
2025-11-19 14:37:59 +0900

    test

---
+++ main.cpp
@@ -0,0 +1,21 @@
+#include <chrono>
+#include <iostream>
+#include "UnionFind.h"
+
+
+int main()
+{
+    auto uf = UnionFind::Generator();
+    auto startFind = std::chrono::high_resolution_clock::now();
+    auto res = uf.Generate(1000, 1000, 8);
+    auto endFind = std::chrono::high_resolution_clock::now();
+    auto milliFind = std::chrono::duration_cast<std::chrono::milliseconds>(endFind - startFind).count();
+    std::cout << "find:" << milliFind << "(" << (float)milliFind / 1000.F << "s)" << "ms\n";
+
+    auto startAnswer = std::chrono::high_resolution_clock::now();
+    auto answer = uf.FindAnser(res);
+    auto endAnswer = std::chrono::high_resolution_clock::now();
+    auto milliAnswer = std::chrono::duration_cast<std::chrono::milliseconds>(endAnswer - startAnswer).count();
+    std::cout << "answer:" << milliAnswer << "(" << (float)milliAnswer / 1000.F << "s)" << "ms\n";
+    return 0;
+}
